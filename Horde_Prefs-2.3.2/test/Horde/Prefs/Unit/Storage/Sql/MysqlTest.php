391f24056c64bcd6527ee5436e4c1cf8f1c36734
Mathieu Parent <math.parent@gmail.com>
2013-04-06 22:38:10 +0200

    Imported Upstream version 2.3.2

--- Horde_Prefs-2.3.2/test/Horde/Prefs/Unit/Storage/Sql/MysqlTest.php
+++ Horde_Prefs-2.3.2/test/Horde/Prefs/Unit/Storage/Sql/MysqlTest.php
@@ -13,19 +13,21 @@ require_once __DIR__ . '/Base.php';
  * @subpackage UnitTests
  * @license    http://www.horde.org/licenses/lgpl21 LGPL 2.1
  */
-class Horde_Prefs_Unit_Storage_Sql_MysqliTest extends Horde_Prefs_Test_Sql_Base
+class Horde_Prefs_Unit_Storage_Sql_MysqlTest extends Horde_Prefs_Test_Sql_Base
 {
     public static function setUpBeforeClass()
     {
-        if (!extension_loaded('mysqli')) {
-            self::$reason = 'No mysqli extension';
+        if (!extension_loaded('mysql')) {
+            self::$reason = 'No mysql extension';
             return;
         }
-        $config = self::getConfig('PREFS_SQL_MYSQLI_TEST_CONFIG',
+        $config = self::getConfig('PREFS_SQL_MYSQL_TEST_CONFIG',
                                   __DIR__ . '/../../..');
-        if ($config && !empty($config['prefs']['sql']['mysqli'])) {
-            self::$db = new Horde_Db_Adapter_Mysqli($config['prefs']['sql']['mysqli']);
+        if ($config && !empty($config['prefs']['sql']['mysql'])) {
+            self::$db = new Horde_Db_Adapter_Mysql($config['prefs']['sql']['mysql']);
             parent::setUpBeforeClass();
+        } else {
+            self::$reason = 'No mysql configuration';
         }
     }
 }
